3dcd326f97326af02cf284a84062b2b5b0b7c560
Christian <Christian@WORKSTATION>
2017-01-31 11:36:27 +0100

    oprettet textboks i view samt bindet boksene til properties

--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -11,6 +11,12 @@ namespace EventMaker.ViewModel
      class EventViewModel
     {
         public EventCatalogSingleton Singleton { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public TimeSpan Time { get; set; }
         public EventViewModel()
         {
             
