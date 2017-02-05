c7c5dee2c2444f42e305068c8f00ebe929ec0638
Christian <Christian@WORKSTATION>
2017-02-05 16:59:35 +0100

    Har lavet Class Diagram

--- EventMaker/Model/EventCatalogSingleton.cs
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -22,15 +22,20 @@ namespace EventMaker.Model
         public ObservableCollection<Event> Events { get; set; }
         private EventCatalogSingleton()
         {
+
             Events = new ObservableCollection<Event>();
 
             Events.Add(new Event(1, "Event", "pool party", "Roskilde", new DateTime(2017, 05, 08)));
             Events.Add(new Event(2, "Event2", "cafe", "Slagelse", new DateTime(2017, 08, 08)));
         }
 
+        public Persistency.PersistencyService pservice { get; set; }
         public void AddEvent(Event eventToAdd)
         {
             Events.Add(eventToAdd);
+
+
+
         }
         public void RemoveEvent(Event eventToRemove)
         {
--- EventMaker/Persistency/PersistencyService.cs
+++ EventMaker/Persistency/PersistencyService.cs
@@ -5,31 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Newtonsoft.Json;
+using EventMaker.Model;
 
 namespace EventMaker.Persistency
 {
     class PersistencyService
     {
-        //public static async void SaveEventAsJsonAsync(ObservableCollection<Model.Event> events)
-        //{
-        //    //string jsonText = Skema.GetJson();
-        //    //StorageFile file = await localfolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
-        //    //await FileIO.WriteTextAsync(file, jsonText);
-        //}
+        public PersistencyService()
+        { }
+        const String fileName = "savedFile.json";
+        public static async void SaveEventAsJsonAsync(ObservableCollection<Model.Event> events)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            string JsonData = JsonConvert.SerializeObject(EventCatalogSingleton.Instance.Events);
+            await FileIO.WriteTextAsync(localFile, JsonData);
 
-        //public static async Task<List<Model.Event>> LoadEventsFromJsonAsync()
-        //{
-            
-        //}
+        }
 
-        //public static async void SerializeEventsFileAsync(string eventsString, string fileName)
-        //{
-            
-        //}
+        public static async Task<List<Model.Event>> LoadEventsFromJsonAsync()
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+            String jsonData = await FileIO.ReadTextAsync(localFile);
+            return JsonConvert.DeserializeObject<List<Event>>(jsonData);
+        }
 
-        //public static async Task<string> DeSerializeEventsFileAsync(String filename)
-        //{
-            
-        //}
+        private static async void SerializeEventsFileAsync(string eventsString, string fileName)
+        {
+        }
+
+        private static async Task<string> DeSerializeEventsFileAsync(String filename)
+        {
+            return filename;
+        }
     }
 }
